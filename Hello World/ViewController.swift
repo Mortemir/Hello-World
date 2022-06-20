8fc999b88aab4d3b4de57b42982a79de3d3b9e47
Mortemir <gesichtsloser@gmail.com>
2022-06-20 17:02:32 +0300

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Ян on 6/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
